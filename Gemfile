52afc46d86813a9c7e21457ed5f153a6e680df32
Thomas Chen <foxnewsnetwork@gmail.com>
2013-01-15 12:04:18 -0800

    gemified this thing with jeweler and rolled in good ol' simplecov to do code coverage

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "http://rubygems.org"
 
 group :development do
+  gem "jeweler"
+  gem "rdoc"
   gem "rspec"
   gem "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
-
+require 'simplecov'
 require File.join File.expand_path("..", File.dirname(__FILE__)), "lib", "cascadence"
+
+SimpleCov.start
